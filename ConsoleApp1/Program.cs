12b8ac49f7d6c743e8f3ca2266f4c3dee1cf053d
LastWord7722 <lastwordmark@gmail.com>
2025-05-22 23:13:23 +0300

    add first version programm

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp1;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Input first number");
+        string firstInput = Console.ReadLine() ?? "0";
+        Console.WriteLine("Input second number");
+        string secondInput = Console.ReadLine() ?? "0";
+        Console.WriteLine(firstInput);
+        Console.WriteLine(secondInput);
+
+        //Int32
+        Console.WriteLine(
+            ToNumber(firstInput) + ToNumber(secondInput)
+        );
+    }
+
+    private static int ToNumber(string? input)
+    {
+        return int.TryParse(input, out var result) ? result : 0;
+    }
+}
